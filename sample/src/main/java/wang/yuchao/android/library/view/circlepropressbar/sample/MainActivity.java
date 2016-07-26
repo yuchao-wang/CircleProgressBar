ec776193ed303183b354ac9514628b4b57c9bfcf
yuchao-wang <1154786190@qq.com>
2016-07-26 16:06:48 +0800

    包名更改

--- sample/src/main/java/wang/yuchao/android/library/view/circlepropressbar/sample/MainActivity.java
+++ sample/src/main/java/wang/yuchao/android/library/view/circlepropressbar/sample/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import wang.yuchao.android.library.view.circleprogressbar.CircleProgressBar;
-import wang.yuchao.android.library.view.sample.circleprogressbar.R;
+import wang.yuchao.android.library.view.circleprogressbar.sample.R;
 
 public class MainActivity extends FragmentActivity {
 
